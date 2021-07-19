690e7c23245cab421ef3358141adac825f804b4d
rigole <foplacide@gmail.com>
2021-07-19 08:24:10 +0100

    checking matching of passwords

--- script.js
+++ script.js
@@ -6,14 +6,29 @@ const message = document.getElementById('message');
 
 
 let isValid = false;
-
+let passwordsMatch = false;
 function validateForm() {
     // Using Constraint API
     isValid = form.checkValidity();
    // Style main message for en error
-    message.textContent =  "Please fill out all the fields";
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    if (!isValid){
+        message.textContent =  "Please fill out all the fields";
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+    }
+   // Check to see if passwords match
+    if (password1El.value === password2El.value){
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    } else{
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+    }
 }
 
 function processFormData(e) {
